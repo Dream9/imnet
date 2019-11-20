80d8a420dd7c812ee82dd3873de9e2106afda733
Dream9 <45119286+Dream9@users.noreply.github.com>
2019-11-20 19:47:03 +0800

    Add files via upload

--- src/test/net/dreamn.cc
+++ src/test/net/dreamn.cc
@@ -13,9 +13,13 @@
 #include"dependent/logger/logger.h"
 #include"dependent/process_information.h"
 #include"dependent/file_extension.h"//FIXME:该封装太简陋了
+#include"response.h"//搜索引擎的相关头文件
+#include"query.h"
 
-#include"unistd.h"
+#include<sys/time.h>
+#include<unistd.h>
 
+#include<time.h>
 #include<limits.h>
 #include<stdlib.h>
 #include<unordered_map>
@@ -28,9 +32,42 @@ using namespace imnet::http;
 const char* work_diretory = "/mnt/share/imnet/doc/http_resource/";
 
 void searcher(const HttpRequest& request, HttpResponse* response);
+void _init_thread_func(Eventloop* loop);
+int _search_response(const string& word, string& response);
+
+//FIXME:
+//brief:全局采用统一的搜索配置
+Query* global_query_agent;
+int page_no = 1;
+const char *rawpath= "/mnt/share/Dreamn/DreamSE/src/doc/raw.test";
+std::unordered_map<int, int>doc_index;
+int total_doc;
+const char *doc_index_path = "/mnt/share/Dreamn/DreamSE/src/doc/doc.index.test";
+unordered_map<string, string>inverse_index;
+const char *inverse_index_file = "/mnt/share/Dreamn/DreamSE/src/doc/inverse.index.test";
+Dictinary dict;
+const char *dict_path = "/mnt/share/Dreamn/DreamSE/src/doc/word.dict";
+score_container ans;
+std::vector<string> words;
+std::vector<float> idf;
 
 int main(int argc, char**argv){
 	LOG_INFO << argv[0] << ",pid = " << process_information::getPid() <<", Listen at 5996.";
+
+
+	///////////////////////////////////////////////////////////
+	//
+	//
+	//初始化查询环境
+	chdir("/mnt/share/Dreamn/DreamSE/src/test/");
+	Query query_agent;
+	query_agent.EnvironmentInit(doc_index,doc_index_path,inverse_index,inverse_index_file);
+	total_doc = doc_index.size();
+	query_agent.GetDictinary(dict, dict_path);
+	global_query_agent = &query_agent;
+	//
+	//
+	///////////////////////////////////////////////////////////
 
 	if(0 != chdir(work_diretory)){
 		LOG_SYSFATAL << "切换工作目录失败";
@@ -45,6 +82,7 @@ int main(int argc, char**argv){
 
 	//测试默认的处理
 	HttpServer server(&loop, "D9Searcher", address);
+	server.setThreadInitCallback(_init_thread_func);
 	server.setThreadNumber(2);
 	server.setServiceCallback(searcher);
 	server.start();
@@ -182,14 +220,71 @@ inline void _file_response(HttpResponse* response, const string& filepath){
 }
 
 //brief:响应cgi请求
-inline void _cgi_response(HttpResponse* response, const string& cgi){
-	//TODO:!!!
-	_500_response(response);
+void _cgi_response(HttpResponse* response, const HttpRequest& request){
+	string cgi = request.getPath().substr(9);
+
+	//检查请求的cgi程序
+	size_t head_len = sizeof "search.cgi" -1;
+	if(cgi.size() < head_len ||
+			0 != memcmp(cgi.c_str(),"search.cgi", head_len )){
+		_404_response(response);
+		return ;
+	}
+
+	//获取结果
+	//if(0!= _search_response(cgi.substr(head_len), response->getBodyHandler())){
+	if(0!= _search_response(request.getQuery(), response->getBodyHandler())){
+		_500_response(response);
+		return ;
+	}
+	
+	response->setStatus(ResponseStatusCode::kOk);
+	response->setContentType("text/html; charset=gbk");
+	response->addHeader("Server","dreamn/imnet");
+}
+
+//brief:查询并响应结果
+int _search_response(const string& word, string& response){
+
+	timeval begin_tv;
+	gettimeofday(&begin_tv,nullptr);
+
+	//FIXME:采用Apache的实现方式？
+	//global_query_agent->GetInput();//原本是针对Apache的CGI 程序
+	int ret = global_query_agent->SetQueryParameter(word);
+	if(ret<0)
+		return -1;
+	
+	global_query_agent->HandleQueryString(dict);
+	global_query_agent->GetAnswer(total_doc,inverse_index,ans,words,idf);
+	timeval end_tv;
+	gettimeofday(&end_tv,nullptr);
+	float used_msec = (end_tv.tv_sec-begin_tv.tv_sec)*1000 
+		+((float)(end_tv.tv_usec-begin_tv.tv_usec))/(float)1000; 
+
+	//结果反馈
+	Response feedback;
+	feedback.ResponseTop(global_query_agent->m_query_string.c_str(), response);
+	feedback.ResponseResult(global_query_agent->m_query_string.c_str(), 
+			ans, page_no, rawpath, doc_index,words,idf, response);
+
+	feedback.ResponseSelectPage(ans.size(),used_msec,1,"456asd1f98sdf", response);
+
+	return 0;
+	//todo:最后的字符串表示缓存的该词的信息id
+
 }
 
 //brief:主业务逻辑
 void searcher(const HttpRequest& request, HttpResponse* response){
 	LOG_INFO << request.MethodToString(request.getMethod()) << request.getPath();
+
+	//TODO
+	if(request.getMethod()!= Method::kGet){
+		//仅保留GET请求
+		_500_response(response);
+		return ;
+	}
 
 	string path = request.getPath();
 	//static_assert(sizeof "/cgi-bin/" == 9, "那我数错了");
@@ -200,7 +295,9 @@ void searcher(const HttpRequest& request, HttpResponse* response){
 		return ;
 	}
 	else if(path.substr(0,9) == "/cgi-bin/"){
-		_cgi_response(response, path.substr(9));
+		//FIXME:request中还含有其他有用信息
+		//_cgi_response(response, path.substr(9));
+		_cgi_response(response, request);
 		return ;
 	}
 	else{
@@ -212,3 +309,7 @@ void searcher(const HttpRequest& request, HttpResponse* response){
 	defaultHttpService(request, response);
 }
 
+//为每个线程进行业务逻辑的初始化
+void _init_thread_func(Eventloop* loop){
+	loop->pushBackIntoLoop(std::bind(chdir,work_diretory));
+}
